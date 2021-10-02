27bc6afc63cc394d6b8d04d0e2cc7b0150e7676f
nelsonmarro99@outlook.com <nelsonmarro99@outlook.com>
2021-10-02 12:41:58 -0500

    Add project files.

---
+++ BlazorKombat/Models/Ability.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorKombat.Models
+{
+    public class Ability
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Controls { get; set; }
+    }
+}
---
+++ BlazorKombat/Models/Fighter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace BlazorKombat.Models
+{
+    public class Fighter
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Bio { get; set; }
+        public MultipartFormDataContent? Sprite { get; set; }
+        public List<Ability>? Abilities { get; set; }
+    }
+}
